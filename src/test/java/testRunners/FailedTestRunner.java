233cd1e01a89860afcbd209501cec459371e30e3
Yusuf Demirci <yusuffdemirci10@gmail.com>
2026-07-15 13:59:12 +0300

    runner class created

--- src/test/java/testRunners/FailedTestRunner.java
+++ src/test/java/testRunners/FailedTestRunner.java
@@ -1,4 +1,17 @@
 package testRunners;
 
-public class FailedTestRunner {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = {"@target/failedrerun.txt"},
+        glue = {"stepDefinitions", "utils"},
+        plugin = {
+                "summary",
+                "pretty",
+                "html:Reports/CucumberReport/FailedReports.html",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+        }
+)
+public class FailedTestRunner extends AbstractTestNGCucumberTests {
 }
--- src/test/java/testRunners/TestRunner.java
+++ src/test/java/testRunners/TestRunner.java
@@ -1,4 +1,21 @@
 package testRunners;
+import io.appium.java_client.AppiumDriver;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-public class TestRunner {
+@CucumberOptions(
+        features ={"src/test/resources/features"},
+        glue = {"stepDefinitions","utils"},
+        tags = "@login",
+        plugin = {
+                "summary",
+                "pretty",
+                "html:Reports/CucumberReport/Reports.html",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "rerun:target/failedrerun.txt"
+        }
+
+)
+
+public class TestRunner extends AbstractTestNGCucumberTests {
 }
